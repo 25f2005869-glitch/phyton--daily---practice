e88826a06a66e1e7f03dbdc1f55915e29566285d
25f2005869-glitch <25f2005869@ds.study.iitm.ac.in>
2026-01-11 16:14:29 +0530

    Add files via upload
    day 1 python practice

---
+++ DAY1,py.py
@@ -0,0 +1,28 @@
+
+#
+
+a = 10
+b = 20
+sum = (a + b)
+print(sum)
+diff = a - b
+print(diff)
+print( a * b)
+
+print( a /b)
+c = 50
+d = 30
+print( c == d)
+print( c >+ d)
+print( c <= d)
+print( not (c > d))
+print(not (c < d))
+val1 = True
+val2 = True
+print("And operator:", val1 and val2)
+val3 = False
+print("or operator;", val1 or val3)
+print("and operator:", val1 and val3)
+s,t = 1,2.5
+sum = s + t
+print(sum)
