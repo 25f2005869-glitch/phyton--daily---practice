44e0963e91e0310a9b2c59e1a03f30d41e275011
Saloni tiwari <25f2005869@ds.study.iitm.ac.in>
2026-01-15 19:19:17 +0530

    Add files via upload
    DAY 4 PRACTICE

---
+++ day4.py
@@ -0,0 +1,67 @@
+str1 = "this is a string."
+str2 = 'apnacollege'
+str3 = """ this is a string"""
+
+str1 = "this is a string.\n we are creating it in a python."
+print(str1)
+
+str1 = "apna"
+str2 = "college"
+print(str1+str2)
+
+str1 = "apna"
+str2 = "college"
+final_str = str1+str2 
+print(final_str)
+
+str1 = "apna"
+len1 = len(str1)
+print(len1)
+
+str2 = "college"
+len2 = len(str2)
+print(len2)
+
+final_str = str1 + " " + str2 
+print(final_str)
+print(len(final_str))
+
+str1 = "this is string."
+len1 = len(str1)
+print(len1)
+
+str2 = "we are studying python."
+len2 = len(str2)
+print(len2)
+
+final_str = str1 + " " + str2 
+print(final_str)
+print(len(final_str))
+
+str1 = "this is a string."
+str2 = 'apnacollege'
+str3 = """ this is a string"""
+print(str1)
+
+str1 = "this is a string."
+str2 = 'apnacollege'
+str3 = """ this is a string"""
+print(str2)
+
+str1 = "this is a string."
+str2 = 'apnacollege'
+str3 = """ this is a string"""
+print(str3)
+
+str1 = "this is string."
+len1 = len(str1)
+print(len1)
+
+str2 = "we are studying python."
+len2 = len(str2)
+print(len2)
+
+final_str = str1 +  str2 
+print(final_str)
+print(len(final_str))
+
