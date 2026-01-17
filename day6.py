5e9dfb4c0bebad1e5880cf2ebc984853e13217c4
Saloni tiwari <25f2005869@ds.study.iitm.ac.in>
2026-01-17 17:51:27 +0530

    Add files via upload
    DAY 6 PRACTICE

---
+++ day6.py
@@ -0,0 +1,48 @@
+str = "i am studying python from apna college"
+print(str.find("o"))
+
+str = "i am studying python from apna college"
+print(str.find("i"))
+
+str = "i am studying python from apna college"
+print(str.find("from"))
+
+str = "i am studying python from apna college"
+print(str.find("q"))
+
+str = "i am studying python from apna college"
+print(str.count("from"))
+
+str = "i am studying python from apna college"
+print(str.count("o"))
+
+name = input("enter your name :")
+print("length of your name is ", len(name))
+
+str = "hi, $iam the $ symbol $99.99"
+print(str.count("$"))
+
+age = input("enter your age :")
+print("length of your age is ", len(age))
+
+marks = input("enter your marks :")
+print("length of your marks is ", len(marks))
+
+age = 21
+
+if(age >= 18):
+    print("can vote and apply for license")
+
+    age = 21
+
+if(age >= 18):
+    print("can vote")
+    print("can drive")
+
+    age = 14
+
+if(age >= 18):
+    print("can vote")
+    print("can drive")
+
+    
