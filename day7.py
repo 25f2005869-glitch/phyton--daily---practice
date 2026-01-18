a9b6479ceaa33b63a8eac3c69b753b0a25a76474
Saloni tiwari <25f2005869@ds.study.iitm.ac.in>
2026-01-18 17:36:30 +0530

    Add files via upload
    DAY 7 PRACTICE

---
+++ day7.py
@@ -0,0 +1,65 @@
+light = "green"
+
+if(light == "red"):
+     print("stop")
+elif(light == "green"):
+    print("go")
+elif(light == "yellow"):
+     print("look")
+
+light = "green"
+
+if(light == "red"):
+     print("stop")
+elif(light == "green"):
+    print("go")
+elif(light == "yellow"):
+     print("look")
+
+print("end of code")
+
+if(num > 2):
+     print("greater than 2")
+if(num > 3):
+     print("greater than 3")
+
+     num = 5
+
+if(num > 2):
+     print("greater than 2")
+elif(num > 3):
+     print("greater than 3")
+
+light = "pink"
+
+if(light == "red"):
+     print("stop")
+elif(light == "green"):
+    print("go")
+elif(light == "yellow"):
+     print("look")
+else:
+   print("light is broken")
+
+print("end of code")
+
+light = "yellow"
+
+if(light == "red"):
+     print("stop")
+elif(light == "green"):
+    print("go")
+elif(light == "yellow"):
+     print("look")
+else:
+   print("light is broken")
+
+print("end of code")
+
+age = 14
+
+if(age >= 18):
+     print("can vote")
+else:
+     print("cannot vote")
+
